91cc873aba0fc498be9ce1fd26d124425857681a
Robert Swanson <swanson7889@vanisd.net>
2019-05-28 19:28:21 -0400

    Made docs README, corrected spelling, corrected dimension comparison

--- math/Matrix.cpp
+++ math/Matrix.cpp
@@ -56,10 +56,10 @@ void Matrix::setDimensions(unsigned int rows, unsigned int cols)
 	}
 }
 
-Matrix Matrix::identity(unsigned int dimentions)
+Matrix Matrix::identity(unsigned int dimensions)
 {
-	Matrix result(dimentions, dimentions, 0);
-	for (int i = 0; i < dimentions; i++) 
+	Matrix result(dimensions, dimensions, 0);
+	for (int i = 0; i < dimensions; i++)
 		result.set(i, i, 1);
 	return result;
 }
@@ -80,9 +80,9 @@ double Matrix::trace() const
 Matrix Matrix::transpose() const
 {
 	Matrix result(cols, rows);
-	
-	for (int r = 0; r < rows; r++) 
-		for (int c = 0; c < cols; c++) 
+
+	for (int r = 0; r < rows; r++)
+		for (int c = 0; c < cols; c++)
 			result.set(c, r, get(r, c));
 
 	return result;
@@ -98,12 +98,12 @@ Matrix& Matrix::map(double (*foo)(double))
 Matrix operator*(const Matrix& left, const Matrix& right)
 {
 	if(left.cols != right.rows)
-		throw std::runtime_error("Invalid dimentions for multiplication!");
+		throw std::runtime_error("Invalid dimensions for multiplication!");
 	Matrix result(left.rows, right.cols);
 
-	for (int r = 0; r < result.rows; r++) 
+	for (int r = 0; r < result.rows; r++)
 	{
-		for (int c = 0; c < result.cols; c++) 
+		for (int c = 0; c < result.cols; c++)
 		{
 			double sum = 0;
 			for (int i = 0; i < left.cols; i++)
@@ -127,14 +127,14 @@ Matrix operator*(const Matrix& left, const int& right)
 
 Matrix operator+(const Matrix& left, const Matrix& right)
 {
-	if(left.size() != right.size())
-		throw std::runtime_error("Invalid dimentions for addition!");
+	if(left.rows == right.rows && left.cols == right.cols)
+		throw std::runtime_error("Invalid dimensions for addition!");
 
 	Matrix result(left.rows, left.cols);
 
-	for (int i = 0; i < result.size(); i++) 
+	for (int i = 0; i < result.size(); i++)
 		result.elements[i] = left.elements[i] + right.elements[i];
-	
+
 	return result;
 }
 
@@ -150,14 +150,14 @@ Matrix operator+(const Matrix& left, const int& right)
 
 Matrix operator-(const Matrix& left, const Matrix& right)
 {
-	if(left.size() != right.size())
-		throw std::runtime_error("Invalid dimentions for subtraction!");
-	
+	if(left.rows == right.rows && left.cols == right.cols)
+		throw std::runtime_error("Invalid dimensions for subtraction!");
+
 	Matrix result(left.rows, left.cols);
 
 	for(int i = 0; i < result.size(); i++)
 		result.elements[i] = left.elements[i] - right.elements[i];
-	
+
 	return result;
 }
 
@@ -177,7 +177,7 @@ Matrix operator^(const Matrix& left, const int& right)
 	result = left;
 
 	for (int i = 0; i < right - 1; i++)
-		result *= left;	
+		result *= left;
 
 	return result;
 }
@@ -202,45 +202,45 @@ Matrix& Matrix::operator*=(const int& other)
 {
 	for (int i = 0; i < size(); i++)
 		elements[i] *= other;
-	
+
 	return *this;
 }
 
 Matrix& Matrix::operator+=(const Matrix& other)
 {
-	if(size() != other.size())
-		throw std::runtime_error("Invalid dimentions for addition!");
+	if(rows == other.rows && cols == other.cols)
+		throw std::runtime_error("Invalid dimensions for addition!");
 
 	for (int i = 0; i < size(); i++)
 		elements[i] += other.elements[i];
-	
+
 	return *this;
 }
 
 Matrix& Matrix::operator+=(const int& other)
 {
-	for (int i = 0; i < std::min(rows, cols); i++) 
+	for (int i = 0; i < std::min(rows, cols); i++)
 		set(i, i, get(i, i) + other);
-	
+
 	return *this;
 }
 
 Matrix& Matrix::operator-=(const Matrix& other)
 {
-	if(size() != other.size())
-		throw std::runtime_error("Invalid dimentions for subtraction!");
-	
+	if(rows == other.rows && cols == other.cols)
+		throw std::runtime_error("Invalid dimensions for subtraction!");
+
 	for (int i = 0; i < size(); i++)
 		elements[i] -= other.elements[i];
-	
+
 	return *this;
 }
 
 Matrix& Matrix::operator-=(const int& other)
 {
-	for (int i = 0; i < std::min(rows, cols); i++) 
+	for (int i = 0; i < std::min(rows, cols); i++)
 		set(i, i, get(i, i) - other);
-	
+
 	return *this;
 }
 
